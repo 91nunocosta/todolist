4b74ad63e5f9094dc73c9219d6d9a65d4201fb2e
nunocosta <91nunocosta@gmail.com>
2021-03-20 13:49:11 +0000

    add trivial unit test

--- tests/test_todolist.py
+++ tests/test_todolist.py
@@ -1,5 +1,18 @@
+import pytest
+
 from todolist import __version__
 
+from todolist.run import app
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_get_root(client):
+    response = client.get('/')
+
+    assert response.status_code == 200
