ca26973ff3875baec4fc52a1a1d26d005337e211
Nuno Costa <91nunocosta@gmail.com>
2021-03-24 17:51:11 +0000

    add api Open API spec generator

---
+++ generate-api-doc.py
@@ -0,0 +1,25 @@
+import json
+from todolist.run import app
+
+DOC_FILE = "open-api-spec.json"
+
+
+def get_open_api_spec():
+    client = app.test_client()
+
+    response = client.get("/api-docs")
+
+    spec = response.json
+
+    return spec
+
+
+def write_spec(spec):
+    dump = json.dumps(spec, indent=4)
+    with open(DOC_FILE, "w") as spec_file:
+        spec_file.write(dump)
+
+
+if __name__ == "__main__":
+    spec = get_open_api_spec()
+    write_spec(spec)
