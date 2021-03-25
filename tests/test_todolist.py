935c5551829a9d79fdc8bff56cfc0c4b0c0ed5a9
Nuno Costa <91nunocosta@gmail.com>
2021-03-25 18:36:51 +0000

    removes test coverage exception

--- tests/test_todolist.py
+++ tests/test_todolist.py
@@ -1,5 +1,17 @@
+from unittest.mock import MagicMock, patch
+
 from todolist import __version__
+from todolist.run import run
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_run():
+    app_mock = MagicMock()
+
+    with patch("todolist.run.create_app", return_value=app_mock):
+        run()
+
+        app_mock.run.assert_called()
